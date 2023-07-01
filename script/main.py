6b91b7ca73bbdb1d0f3a44bbe68bb2693fdb4a5c
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2023-07-02 03:49:02 +0900

    sampleファイルの追加

--- script/main.py
+++ script/main.py
@@ -1,3 +1,9 @@
-from models.logistic_regression import LogisticRegression
+from dataset.Norm2dDataset import Norm2dParameter, Norm2dDataset
 
-print(LogisticRegression.__name__)
+if __name__ == "__main__":
+    one = Norm2dParameter((0, 0), ((1, 0), (0, 1)), 1000)
+    two = Norm2dParameter((0, 0), ((1, 0), (0, 1)), 1000)
+
+    dataset = Norm2dDataset((one, two))
+    print(dataset.X_COLUMNS)
+    df = dataset.X
