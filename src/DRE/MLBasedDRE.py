dde4a78a64c721e1080d499e5b5b809959cbd04f
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2023-08-07 00:55:08 +0900

    [Refactor] use model factory class

--- src/DRE/MLBasedDRE.py
+++ src/DRE/MLBasedDRE.py
@@ -1,9 +1,9 @@
 from DRE.DRE import Parameter as DRE_Parameter, DRE
 from typing import Union
-from models.model_wrapper import ModelParameter
+from models.modelBase import ModelParameter
 from dataset.dataset import Dataset
 from dataclasses import dataclass
-from models import factory
+from models.modelFactory import ModelFactory
 
 
 @dataclass(frozen=True)
@@ -15,7 +15,6 @@ class MLBasedDRE_Parameter(DRE_Parameter):
         _description_
     """
 
-    model_name: str
     model_param: Union[ModelParameter, dict]
 
 
@@ -26,13 +25,15 @@ class MLBasedDRE(DRE):
         self.parameter = MLBasedDRE_Parameter.from_dict(param)
 
     def estimate(self, dataset: Dataset) -> float:
-        # model learning and pridict
-        self.model = factory.new_model_instance("LogisticRegression", self.parameter.model_param)
+        self.model = ModelFactory.create(self.parameter.model_param)
         self.model.train(dataset.X, dataset.Y)
 
-        dataset["rprob"] = self.model.predict_proba(dataset.X)[:, 1].round(1)  # クラス1に含む確率
+        dataset["rprob"] = self.model.predict_proba(dataset.X).reshape(-1).round(2)  # クラス1に含む確率
         dataset["inv_rprob"] = 1 - dataset["rprob"]  # クラス2に含む確率
 
         dataset["DRE"] = dataset["rprob"] / dataset["inv_rprob"].clip(upper_bound=2, lower_bound=self.MIN_CORERCTION)
+        y1_size = dataset.count_value("y", 1)
+        y0_size = dataset.count_value("y", 0)
 
+        dataset["DRE"] = dataset["DRE"] * y1_size / y0_size
         return dataset["DRE"].mean()
