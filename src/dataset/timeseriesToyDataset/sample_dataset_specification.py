22cfb0acefd81966e77a40c4fec7895241442bdf
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2023-08-31 01:04:15 +0900

    add dataset

--- src/dataset/timeseriesToyDataset/sample_dataset_specification.py
+++ src/dataset/timeseriesToyDataset/sample_dataset_specification.py
@@ -16,6 +16,7 @@ def add_shifts_to_linear_params(
 ToyDatasetProblem = [
     (LinearParameter(0, 5, 0.1, 10000), LinearParameter(0, 14, 0.1, 10000)),
     (LinearParameter(0.0005, 2, 0.1, 10000), LinearParameter(0.0005, 10, 0.1, 10000)),
+    (LinearParameter(0.001, 2, 0.1, 10000), LinearParameter(0.001, 4, 0.1, 10000)),
     (LinearParameter(0, 3, 0.1, 10000), LinearParameter(0.001, 3, 0.1, 10000)),
     (LinearParameter(0.001, 2, 0.1, 10000), LinearParameter(0, 12, 0.1, 10000)),
     (LinearParameter(0.0001, 2, 0.1, 10000), LinearParameter(0.001, 3, 0.1, 10000)),
